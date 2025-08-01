ffc39093d135d5804252eefaa56448b00158f0f8
Quantumania23 <mikepeace981@gmail.com>
2025-08-01 15:12:24 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -149,6 +149,18 @@ export default function RootLayout({
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2499617146577250"
      crossOrigin="anonymous"></script>
         
+{/* Google Tag Manager */}
+<script
+  dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KTZZVS56');`
+  }}
+/>
+{/*End Google Tag Manager  */} 
+
         {/* Geographic targeting */}
         <meta name="geo.region" content="KE" />
         <meta name="geo.placename" content="Nairobi" />
@@ -251,6 +263,16 @@ export default function RootLayout({
         />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
+         {/* Google Tag Manager (noscript) */}
+<noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-KTZZVS56"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  ></iframe>
+</noscript>
+ {/* End Google Tag Manager (noscript)  */}
         <ThemeProvider attribute="class" defaultTheme="dark">
           <CursorSpotlight />
           {children}
